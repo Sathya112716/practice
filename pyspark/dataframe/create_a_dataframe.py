a8cce1c011e828e32505ca949049614a391f3bd2
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-22 21:15:46 +0530

    Create a new dataframe and display it

--- pyspark/dataframe/create_a_dataframe.py
+++ pyspark/dataframe/create_a_dataframe.py
@@ -4,7 +4,7 @@ from pyspark.sql import Row
 # Create a SparkSession
 spark = SparkSession.builder.appName("DataFrame Creation").getOrCreate()
 
-# Method 1: Create DataFrame from an existing collection (e.g., list of tuples)
+# Method 1: Create DataFrame from an existing collection
 data = [("Sathya", 24), ("Eniyan", 16), ("Ambika", 50)]
 df1 = spark.createDataFrame(data, ["Name", "Age"])
 print("DataFrame 1:")
