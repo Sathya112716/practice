e50b43bcce9351e068727d45192b98dbdd02f867
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-25 21:37:42 +0530

    Use joins for two dataframe

---
+++ pyspark/dataframe/joins_two_datframe.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("DataFrame Creation").getOrCreate()
+
+# Method 1: Create DataFrame from an existing collect
+emp= [(1,"Sathya", 24), (2,"Eniyan",16), (3,"Ambika", 50),(4,"Ravi",55),(5,"Priya",25)]
+df1 = spark.createDataFrame(emp, ["emp_id","Name", "Age"])
+print("DataFrame 1:",df1)
+df1.show()
+
+dept=[(1,"Marketing"),(2,"Finance"),(3,"Sales")]
+df2=spark.createDataFrame(dept,["dept_id","dept_name"])
+print("DataFrame 2:",df2)
+df2.show()
+
+df1.join(df2,df1.emp_id==df2.dept_id,"inner").show()
+
+df1.join(df2,df1.emp_id==df2.dept_id,"left").show()
+
+df1.join(df2,df1.emp_id==df2.dept_id,"right").show()
+
+df1.join(df2,df1.emp_id==df2.dept_id,"outer").show()
+
+df1.join(df2,df1.emp_id==df2.dept_id,"leftanti").show()
+
+df1.join(df2,df1.emp_id==df2.dept_id,"leftsemi").show()
+spark.stop()
