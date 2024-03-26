98b9ab7227a1359da7b449e35e64cacd79aeedfe
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-26 12:40:14 +0530

    Create a rdd and convert it to dataframe using two methods

---
+++ pyspark/dataframe/rdd_to_dataframe.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("RDD Creation").getOrCreate()
+sc=spark.sparkContext
+data=[("Sathya",24),("Ravi",45),("priya",17)]
+
+rdd=sc.parallelize(data)
+print(rdd)
+df=rdd.toDF()
+df.printSchema()
+df.show()
+
+df1=spark.createDataFrame(rdd,["Name","Age"])
+df1.printSchema()
+df1.show()
