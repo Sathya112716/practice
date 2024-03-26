5d6acf857f9c0ff7fc8b3a65e09dd5f14d3bc2ab
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-26 18:13:53 +0530

    Cast the column to the different datatype

---
+++ pyspark/dataframe/cast_function.py
@@ -0,0 +1,18 @@
+
+from pyspark.sql.functions import col
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Cast Function").getOrCreate()
+sc = spark.sparkContext
+
+data= [("Sathya", "24", 1), ("Ravi", "45", 2), ("priya", "17", 3), ("Anu", "18", 4), ("Eniyan", "34", 5), ("Sandhya", "56", 6), ("Bhuvanesh", "25", 7)]
+
+rdd=sc.parallelize(data)
+print(rdd)
+df1=spark.createDataFrame(rdd,["Name","Age","Id"])
+print("Before Casting:")
+df1.printSchema()
+
+df1_casted=df1.withColumn("Age",col("Age").cast("int"))
+print("After casting:")
+df1.printSchema()
