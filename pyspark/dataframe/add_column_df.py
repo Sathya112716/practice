f8a7d27e7f7810ff96cf840ece6a58ad5bffac95
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-27 12:25:26 +0530

    Create a dataframe and add column to the df

--- pyspark/dataframe/add_column_df.py
+++ pyspark/dataframe/add_column_df.py
@@ -1,14 +1,10 @@
 from pyspark.sql import SparkSession
-from pyspark.sql import Row
-from pyspark.sql.functions import lit, when
-# Create a new SparkSession
+from pyspark.sql.functions import lit
 spark = SparkSession.builder.appName("Add Columns Example").getOrCreate()
 
-# Create a Df
 data = [("John", 25), ("Alice", 30), ("Bob", 35)]
 df = spark.createDataFrame(data, ["Name", "Age"])
 
-# Display the frame
 print("Original DataFrame:")
 df.show()
 
@@ -18,7 +14,6 @@ df_with_city = df.withColumn("City",lit("New York"))
 from pyspark.sql.functions import when
 df_with_gender = df_with_city.withColumn("Gender", when(df["Name"] == "John", "Male").otherwise("Female"))
 
-# Display
 print("DataFrame with added columns:")
 df_with_gender.show()
 
