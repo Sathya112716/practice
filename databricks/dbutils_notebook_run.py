db6df85a82b733b0eca0d19713289fc716b90271
Sathya112716 <sathyapriya112716@gmail.com>
2024-04-11 21:34:57 +0530

    dbutils notebook run command

--- databricks/dbutils_notebook_run.py
+++ databricks/dbutils_notebook_run.py
@@ -2,13 +2,9 @@
 firstname='Sathya'
 dbutils.notebook.exit(firstname)
 
-
-# COMMAND ----------
-
 lastname='Priya'
 print(lastname)
 
-# COMMAND ----------
 
 var1=dbutils.notebook.run('/Users/sathyapriya.r@diggibyte.com/Untitled Notebook 2024-04-11 21:02:57',60)
 print(var1)
