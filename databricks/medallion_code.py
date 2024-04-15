d4a2de3c704f4d89179ae07f72bd111bcaf5818b
Sathya112716 <sathyapriya112716@gmail.com>
2024-04-15 21:59:04 +0530

    Perform some aggregate functions in the gold layer

--- databricks/medallion_code.py
+++ databricks/medallion_code.py
@@ -1,4 +1,4 @@
-# Databricks notebook source
+# Databricks notebook source file
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Medallion Data Processing").getOrCreate()
